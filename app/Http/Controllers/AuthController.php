446f3f9dbbd4483d8b5f0c45825460cfcc4a549a
Link1515 <h26281660@gmail.com>
2025-06-03 17:59:20 +0800

    feat: introduce simple authentication flow by Laravel Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        User::create($validated);
+
+        return response()->noContent();
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email'    => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!auth()->attempt($validated)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 422);
+        }
+
+        $user = auth()->user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}

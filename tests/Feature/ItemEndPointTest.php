eaf11bb294d7603a2546b489f0b20e1ae547680a
Link1515 <h26281660@gmail.com>
2025-06-11 18:12:19 +0800

    test: add test for item store endpoint

---
+++ tests/Feature/ItemEndPointTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ItemEndPointTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_can_create_item()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $payload = [
+            "name"    => "egg",
+            "protein" => 12
+        ];
+
+        $response = $this->postJson('/api/item', $payload);
+        $response->assertStatus(204);
+        $this->assertDatabaseHas('items', [
+            "name"    => "egg",
+            "protein" => 12
+        ]);
+    }
+}

b09994c1132194754c54a6689bdd13a2afaa24ee
Link1515 <h26281660@gmail.com>
2025-06-20 17:46:00 +0800

    test: add test for item update endpoint

--- tests/Feature/ItemEndPointTest.php
+++ tests/Feature/ItemEndPointTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Item;
+use App\Models\Record;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
@@ -28,6 +30,28 @@ class ItemEndPointTest extends TestCase
         $this->assertDatabaseHas('items', [
             "name"    => "egg",
             "protein" => 12
+        ]);
+    }
+
+    #[Test]
+    public function it_can_update_item()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $record = Record::factory()->for($user)->has(Item::factory()->state(['name' => 'Chicken', 'protein' => 30.5]))->create();
+        $item   = $record->items->first();
+
+        $payload = [
+            "protein" => 40
+        ];
+
+        $response = $this->patchJson('/api/item/' . $item->id, $payload);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('items', [
+            'name'      => 'Chicken',
+            'protein'   => 40,
+            'record_id' => $record->id
         ]);
     }
 }

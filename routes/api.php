4cd670ea4d956f5b9a591331c0ae5089a1262596
Link1515 <h26281660@gmail.com>
2025-07-06 13:26:02 +0800

    chore: redirect / and /api to /api/documentation

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/api/documentation');
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 

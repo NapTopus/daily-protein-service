9c57e70fcf0f173d9b9a243a0f3f62bc511ce021
Link1515 <h26281660@gmail.com>
2025-07-23 18:01:43 +0800

    feat: enable dark theme support for api docs view

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -25,98 +25,171 @@
       background: #fafafa;
     }
     </style>
-    @if(config('l5-swagger.defaults.ui.display.dark_mode'))
-        <style>
-            body#dark-mode,
-            #dark-mode .scheme-container {
-                background: #1b1b1b;
-            }
-            #dark-mode .scheme-container,
-            #dark-mode .opblock .opblock-section-header{
-                box-shadow: 0 1px 2px 0 rgba(255, 255, 255, 0.15);
-            }
-            #dark-mode .operation-filter-input,
-            #dark-mode .dialog-ux .modal-ux,
-            #dark-mode input[type=email],
-            #dark-mode input[type=file],
-            #dark-mode input[type=password],
-            #dark-mode input[type=search],
-            #dark-mode input[type=text],
-            #dark-mode textarea{
-                background: #343434;
-                color: #e7e7e7;
-            }
-            #dark-mode .title,
-            #dark-mode li,
-            #dark-mode p,
-            #dark-mode table,
-            #dark-mode label,
-            #dark-mode .opblock-tag,
-            #dark-mode .opblock .opblock-summary-operation-id,
-            #dark-mode .opblock .opblock-summary-path,
-            #dark-mode .opblock .opblock-summary-path__deprecated,
-            #dark-mode h1,
-            #dark-mode h2,
-            #dark-mode h3,
-            #dark-mode h4,
-            #dark-mode h5,
-            #dark-mode .btn,
-            #dark-mode .tab li,
-            #dark-mode .parameter__name,
-            #dark-mode .parameter__type,
-            #dark-mode .prop-format,
-            #dark-mode .loading-container .loading:after{
-                color: #e7e7e7;
-            }
-            #dark-mode .opblock-description-wrapper p,
-            #dark-mode .opblock-external-docs-wrapper p,
-            #dark-mode .opblock-title_normal p,
-            #dark-mode .response-col_status,
-            #dark-mode table thead tr td,
-            #dark-mode table thead tr th,
-            #dark-mode .response-col_links,
-            #dark-mode .swagger-ui{
-                color: wheat;
-            }
-            #dark-mode .parameter__extension,
-            #dark-mode .parameter__in,
-            #dark-mode .model-title{
-                color: #949494;
-            }
-            #dark-mode table thead tr td,
-            #dark-mode table thead tr th{
-                border-color: rgba(120,120,120,.2);
-            }
-            #dark-mode .opblock .opblock-section-header{
-                background: transparent;
-            }
-            #dark-mode .opblock.opblock-post{
-                background: rgba(73,204,144,.25);
-            }
-            #dark-mode .opblock.opblock-get{
-                background: rgba(97,175,254,.25);
-            }
-            #dark-mode .opblock.opblock-put{
-                background: rgba(252,161,48,.25);
-            }
-            #dark-mode .opblock.opblock-delete{
-                background: rgba(249,62,62,.25);
-            }
-            #dark-mode .loading-container .loading:before{
-                border-color: rgba(255,255,255,10%);
-                border-top-color: rgba(255,255,255,.6);
-            }
-            #dark-mode svg:not(:root){
-                fill: #e7e7e7;
-            }
-            #dark-mode .opblock-summary-description {
-                color: #fafafa;
-            }
-        </style>
-    @endif
+    <style>
+        body#dark-mode,
+        #dark-mode .scheme-container {
+            background: #1b1b1b;
+        }
+        #dark-mode .scheme-container,
+        #dark-mode .opblock .opblock-section-header{
+            box-shadow: 0 1px 2px 0 rgba(255, 255, 255, 0.15);
+        }
+        #dark-mode .operation-filter-input,
+        #dark-mode .dialog-ux .modal-ux,
+        #dark-mode input[type=email],
+        #dark-mode input[type=file],
+        #dark-mode input[type=password],
+        #dark-mode input[type=search],
+        #dark-mode input[type=text],
+        #dark-mode textarea{
+            background: #343434;
+            color: #e7e7e7;
+        }
+        #dark-mode .title,
+        #dark-mode li,
+        #dark-mode p,
+        #dark-mode table,
+        #dark-mode label,
+        #dark-mode .opblock-tag,
+        #dark-mode .opblock .opblock-summary-operation-id,
+        #dark-mode .opblock .opblock-summary-path,
+        #dark-mode .opblock .opblock-summary-path__deprecated,
+        #dark-mode h1,
+        #dark-mode h2,
+        #dark-mode h3,
+        #dark-mode h4,
+        #dark-mode h5,
+        #dark-mode .btn,
+        #dark-mode .tab li,
+        #dark-mode .parameter__name,
+        #dark-mode .parameter__type,
+        #dark-mode .prop-format,
+        #dark-mode .loading-container .loading:after{
+            color: #e7e7e7;
+        }
+        #dark-mode .opblock-description-wrapper p,
+        #dark-mode .opblock-external-docs-wrapper p,
+        #dark-mode .opblock-title_normal p,
+        #dark-mode .response-col_status,
+        #dark-mode table thead tr td,
+        #dark-mode table thead tr th,
+        #dark-mode .response-col_links,
+        #dark-mode .swagger-ui{
+            color: wheat;
+        }
+        #dark-mode .parameter__extension,
+        #dark-mode .parameter__in,
+        #dark-mode .model-title{
+            color: #949494;
+        }
+        #dark-mode table thead tr td,
+        #dark-mode table thead tr th{
+            border-color: rgba(120,120,120,.2);
+        }
+        #dark-mode .opblock .opblock-section-header{
+            background: transparent;
+        }
+        #dark-mode .opblock.opblock-post{
+            background: rgba(73,204,144,.25);
+        }
+        #dark-mode .opblock.opblock-get{
+            background: rgba(97,175,254,.25);
+        }
+        #dark-mode .opblock.opblock-put{
+            background: rgba(252,161,48,.25);
+        }
+        #dark-mode .opblock.opblock-delete{
+            background: rgba(249,62,62,.25);
+        }
+        #dark-mode .loading-container .loading:before{
+            border-color: rgba(255,255,255,10%);
+            border-top-color: rgba(255,255,255,.6);
+        }
+        #dark-mode svg:not(:root){
+            fill: #e7e7e7;
+        }
+        #dark-mode .opblock-summary-description {
+            color: #fafafa;
+        }
+    </style>
+
+    <script src="https://code.iconify.design/iconify-icon/3.0.0/iconify-icon.min.js"></script>
+
+    <style>
+        .toggle-theme-btn {
+            all: unset;
+            cursor: pointer;
+            position: fixed;
+            right: 1rem;
+            bottom: 1rem;
+            width: 40px;
+            height: 40px;
+            border-radius: 9999px;
+            background-color: white;
+            display: grid;
+            place-items: center;
+            box-shadow: 0px 0px 4px -1px rgba(0,0,0,0.2)
+        }
+
+        .toggle-theme-btn.dark {
+            background-color: black;
+            box-shadow: 0px 0px 4px 1px rgba(255,255,255,0.2)
+        }
+
+        .toggle-theme-btn .icon {
+            width: 28px;
+            height: 28px;
+        }
+
+        .toggle-theme-btn .dark-icon {
+            display: none;
+        }
+
+        .toggle-theme-btn.dark .dark-icon {
+            display: block;
+        }
+
+        .toggle-theme-btn.dark .light-icon {
+            display: none;
+        }
+    </style>
 </head>
 
-<body @if(config('l5-swagger.defaults.ui.display.dark_mode')) id="dark-mode" @endif>
+<body>
+
+<button id="toggleThemeBtn" class="toggle-theme-btn">
+    <span class="icon light-icon">
+        <iconify-icon icon="stash:sun-solid" width="28" height="28"  style="color: black"></iconify-icon>
+    </span>
+    <span class="icon dark-icon">
+        <iconify-icon icon="stash:moon-solid" width="28" height="28"  style="color: white"></iconify-icon>
+    </span>
+</button>
+
+<script>
+    let defaultDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (localStorage.getItem('defaultTheme') !== null) {
+        defaultDarkMode = localStorage.getItem('defaultTheme') === 'dark'
+    }
+    const toggleThemeBtnEl = document.querySelector('#toggleThemeBtn');
+    if (defaultDarkMode) {
+        document.body.id = 'dark-mode';
+        toggleThemeBtnEl.classList.add('dark');
+    }
+
+    toggleThemeBtnEl.addEventListener('click', () => {
+        if (document.body.id === 'dark-mode') {
+            document.body.id = '';
+            toggleThemeBtnEl.classList.remove('dark');
+            localStorage.setItem('defaultTheme', 'light')
+        } else {
+            document.body.id = 'dark-mode';
+            toggleThemeBtnEl.classList.add('dark');
+            localStorage.setItem('defaultTheme', 'dark')
+        }
+    });
+</script>
+
 <div id="swagger-ui"></div>
 
 <script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>

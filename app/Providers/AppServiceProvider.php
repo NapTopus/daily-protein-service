ab2c789cc1cb0e36d87fca6fe44a5ca4b6a02e6c
Link1515 <h26281660@gmail.com>
2025-11-16 17:34:40 +0800

    chore: formatting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,14 +24,14 @@ class AppServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function (Request $request) {
             $userKey = optional($request->user())->getAuthIdentifier();
-            $key     = $userKey ? "uid:{$userKey}" : "ip:" . $request->ip();
+            $key     = $userKey ? "uid:{$userKey}" : 'ip:' . $request->ip();
 
             return Limit::perMinute(120)->by($key);
         });
 
         RateLimiter::for('writes', function (Request $request) {
             $userKey = optional($request->user())->getAuthIdentifier();
-            $key     = $userKey ? "uid:{$userKey}" : "ip:" . $request->ip();
+            $key     = $userKey ? "uid:{$userKey}" : 'ip:' . $request->ip();
 
             return Limit::perMinute(30)->by($key);
         });
